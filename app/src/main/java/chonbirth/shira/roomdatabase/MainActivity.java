30facfee13f49bebf605d2615288002ecfac740e
Chonbirth Shira <chonbirth@gmail.com>
2021-07-20 08:39:02 +0530

    Initial commit

---
+++ app/src/main/java/chonbirth/shira/roomdatabase/MainActivity.java
@@ -0,0 +1,14 @@
+package chonbirth.shira.roomdatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
